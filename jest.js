3ee46c4e9801b9e0ecec7173a72ba5e3ac0c67f4
ericksarabia <erick.sarabia@outlook.com>
2022-01-03 17:36:36 -0600

    Add jest rules

---
+++ jest.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+        "jest/globals": true,
+    },
+    plugins: ["jest"],
+    extends: ["plugin:jest/recommended", "plugin:jest/style"],
+    rules: {
+        "no-unused-expressions": 0,
+    },
+};
